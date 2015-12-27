f35859c3235c750d6a97778b0fa90ecbee6efc43
Eleanor Weigert <eweigert@connectivity.com>
2015-12-26 22:21:51 -0800

    preliminary styling of search results

--- SearchResults.js
+++ SearchResults.js
@@ -12,6 +12,34 @@ var {
     Component
 } = React;
 
+var styles = StyleSheet.create({
+    thumb: {
+        width: 80,
+        height: 80,
+        marginRight: 10
+    },
+    textContainer: {
+        flex: 1
+    },
+    separator: {
+        height: 1,
+        backgroundColor: '#dddddd'
+    },
+    number: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        color: '#48BBEC'
+    },
+    title: {
+        fontSize: 20,
+        color: '#656565'
+    },
+    rowContainer: {
+        flexDirection: 'row',
+        padding: 10
+    }
+});
+
 class SearchResults extends Component {
 
     constructor(props) {
@@ -23,12 +51,26 @@ class SearchResults extends Component {
         };
     }
 
+    rowPressed(property) {
+        var property = this.props.results.filter(prop => prop === property);
+    }
+
     renderRow(rowData, sectionID, rowID) {
+        var number = rowData.phone;
         return (
-            <TouchableHighlight
+            <TouchableHighlight 
+                onPress={() => this.rowPressed(rowData)}
                 underlayColor='#dddddd'>
             <View>
-                <Text>{rowData}</Text>
+                <View style={styles.rowContainer}>
+                    <Image style={styles.thumb} source={{ uri: rowData.image_url }} />
+                    <View style={styles.textContainer}>
+                        <Text style={styles.number}>{number}</Text>
+                        <Text style={styles.title}
+                            numberOfLines={1}>{rowData.name}</Text>
+                    </View>
+                </View>
+                <View style={styles.separator}/>
             </View>
             </TouchableHighlight>
         );
