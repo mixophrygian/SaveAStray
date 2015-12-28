870fce984c8a1bf6e2baf53568a70c0a62e3ed2e
Eleanor Weigert <eweigert@connectivity.com>
2015-12-27 19:44:18 -0800

    Search page restyle, first serious version

--- SearchPage.js
+++ SearchPage.js
@@ -64,11 +64,14 @@ class SearchPage extends Component {
                 size='large'/>) :
             (<View style={styles.spinnerPlaceHolder}/>);
         return (
-            <View style={styles.container}>
-                <Text style={styles.title}>
-                   Rescue a lost friend 
+        <Image style={styles.container} source={require('./catnose3.png')}>
+            <View style={styles.content}>
+                <Text style={styles.bigTitle}>
+                   Rescue 
                 </Text>
-               <Image source={require('image!doghelp')} style={styles.image}/>
+                <Text style={styles.title}>
+                    a lost pet 
+                </Text>
                 {spinner}
                     <View style={styles.flowRight}>
                     <TextInput
@@ -88,10 +91,10 @@ class SearchPage extends Component {
                      <Text style={styles.buttonText}>Current Location</Text>
                </TouchableHighlight> 
                 <Text style={styles.description}>
-                    Search for no-kill shelters by zipcode or current location.
                 </Text>
                 <Text style={styles.description}>{this.state.message}</Text>
             </View>
+            </Image>
         );
     }
 };
@@ -99,19 +102,46 @@ class SearchPage extends Component {
 var ButtonInputHeight = 46;
                 
 var styles = StyleSheet.create({
-    title: {
-        fontSize: 38,
+    bigTitle: {
+        fontSize: 74,
         textAlign: 'center',
-        color: '#656565'
+        color: 'white',
+        shadowColor: 'black',
+        shadowOffset: {width: 0, height: 0},
+        shadowOpacity: 1,
+        shadowRadius: 10
+    },
+    title: {
+        fontSize: 48,
+        textAlign: 'center',
+        color: 'white',
+        shadowColor: 'black',
+        marginTop: -15,
+        marginBottom: 10,
+        shadowOffset: {width: 0, height: 0},
+        shadowOpacity: 1,
+        shadowRadius: 8
     },
     description: {
-        fontSize: 18,
+        fontSize: 0,
         textAlign: 'center',
-        color: '#656565'
+        color: 'white',
+        shadowColor: 'black',
+        shadowOffset: {width: 0, height: 0},
+        shadowOpacity: 1,
+        shadowRadius: 6
     },
     container: {
+        flex: 1,
+        width: null,
+        height: null,
+        resizeMode: 'cover'
+    },
+    content: {
         padding: 30,
-        marginTop: 45,
+        marginTop:35,
+        backgroundColor: 'rgba(0,0,0,0)',
+        flexDirection: 'column',
         alignItems: 'center'
     },
     flowRight: {
@@ -125,11 +155,11 @@ var styles = StyleSheet.create({
         alignSelf: 'center'
     },
     button: {
-        height: ButtonInputHeight,
         flex: 1,
         flexDirection: 'row',
-        backgroundColor: '#48BBEC',
-        borderColor: '#48BBEC',
+        height: ButtonInputHeight,
+        backgroundColor: 'rgba(1,1,1,0.7)',
+        borderColor: 'white',
         borderWidth: 1,
         borderRadius: 8,
         marginBottom: 10,
@@ -143,16 +173,11 @@ var styles = StyleSheet.create({
         flex: 4,
         fontSize: 20,
         borderWidth: 1,
-        borderColor: '#48BBEC',
+        borderColor: 'white',
+        backgroundColor: 'rgba(1,1,1,0.4)',
         borderRadius: 8,
-        color: 'black',
+        color: 'white',
         paddingLeft: 15
-    },
-    image: {
-        width: 215,
-        height: 200,
-        margin: 10,
-        marginBottom: 40
     },
     spinnerPlaceHolder: {
     },
@@ -161,3 +186,5 @@ var styles = StyleSheet.create({
 });
 
 module.exports = SearchPage;
+
+               //<Image source={require('image!doghelp')} style={styles.image}/>
