ef2890ab22c57eac976cecb523529a9b8c585e21
Eleanor Weigert <eweigert@gmail.com>
2016-04-07 22:04:14 -0700

    remove spurious images/ folder and update SearchPage / Launch Image to use new photograph of tan puppy

--- app/SearchPage.js
+++ app/SearchPage.js
@@ -108,7 +108,7 @@ class SearchPage extends Component {
                 size='large'/>) :
             (<View style={styles.indicator}/>);
         return (
-        <Image style={styles.container} source={{ uri: 'crosswalk', isStatic: true}}>
+        <Image style={styles.container} source={{ uri: 'tanpuppy', isStatic: true}}>
             <View style={[styles.content, {marginBottom: this.state.keyboardMargin}]}>
                 <Text style={styles.bigTitle}>
                   Save a </Text>
